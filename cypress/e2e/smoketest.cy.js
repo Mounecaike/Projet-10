d94cda9b5a7883bd5d589d5de2a8306f4d9bf4fa
Mounecaike <jordanl4213@gmail.com>
2024-11-21 14:15:42 +0100

    Update smoketest.cy.js

--- cypress/e2e/smoketest.cy.js
+++ cypress/e2e/smoketest.cy.js
@@ -9,6 +9,16 @@ describe('Smoke Tests', () => {
     cy.title().should('include', 'EcoBlissBath'); // Vérifie le titre de la page
   });
 
+  it('vérifie la présence des boutons', () => {
+    cy.visit('/');
+    cy.get('[data-cy="nav-link-home"]').should('be.visible');
+    cy.get('[data-cy="nav-link-products"]').should('be.visible');
+    cy.get('[data-cy="nav-link-reviews"]').should('be.visible');
+    cy.get('[data-cy="nav-link-home-logo"]').should('be.visible');
+    cy.get('[data-cy="nav-link-login"]').should('be.visible');
+    cy.get('[data-cy="nav-link-register"]').should('be.visible');
+  })
+
   it('Permet la connexion d\'un utilisateur', () => {
     cy.visit('/login');
     cy.get('[data-cy="login-input-username"]').type('test2@test.fr'); // Saisit l'email
@@ -17,6 +27,7 @@ describe('Smoke Tests', () => {
     cy.url().should('include', '/'); // Vérifie la redirection
     cy.window().its('localStorage.authToken').should('exist');
   });
+
 
   it('Vérifie la navigation vers une page principale', () => {
     cy.get('nav a[data-cy="nav-link-products"]').click(); // Clique sur le lien vers "À propos"
@@ -28,7 +39,7 @@ describe('Smoke Tests', () => {
     cy.get('[data-cy="product-link"]').should('be.visible');
     });
 
-      
+       
   it('Laissez un avis', () => {
     cy.login();
     cy.get('[data-cy="nav-link-logout"]').should('be.visible');
@@ -44,4 +55,26 @@ describe('Smoke Tests', () => {
     cy.contains(reviewTitle).should('be.visible');
     cy.contains(reviewComment).should('be.visible');
     });
+
+  it('page produits', () => {
+    cy.visit('/products');
+    cy.get('[data-cy="product-link"]').eq(0).click();
+    cy.url().should('include', '/products');
+    const selectors = [
+      '[data-cy="detail-product-img"]',
+      '[data-cy="detail-product-name"]',
+      '[data-cy="detail-product-description"]',
+      '[data-cy="detail-product-skin"]',
+      '[data-cy="detail-product-aromas"]',
+      '[data-cy="detail-product-ingredients"]',
+      '[data-cy="detail-product-form"]',
+      '[data-cy="detail-product-price"]',
+      '[data-cy="detail-product-stock"]',
+      '[data-cy="detail-product-quantity"]',
+      '[data-cy="detail-product-add"]',
+    ];
+    selectors.forEach((selectors) => {
+      cy.get(selectors).should('be.visible');
+    });
+  })
 });
