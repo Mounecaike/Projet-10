8229ede19cd0224e6b893338c79323886e0293ab
Mounecaike <jordanl4213@gmail.com>
2024-11-24 12:08:45 +0100

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,4 +31,21 @@ Cypress.Commands.add('login', () => {
     cy.get('[data-cy="login-submit"]').click();
     cy.url().should('include', '/');
   });
+  Cypress.Commands.add('getAuthToken', () => {
+    return 'fake-token-test'; // Remplace par une commande réelle si nécessaire
+  });
+  
+  Cypress.Commands.add('apiRequest', (method, url, body = null) => {
+    const token = Cypress.env('authToken'); // Récupère le token stocké
+  
+    return cy.request({
+      method,
+      url: `${Cypress.env('apiBaseUrl')}${url}`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body,
+      failOnStatusCode: false, // Permet d'inspecter les réponses même en cas d'erreur
+    });
+  });
   
