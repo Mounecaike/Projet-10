a22924c3ab8be3a200d3a06bc8d4a8074256197e
Mounecaike <jordanl4213@gmail.com>
2024-11-27 13:23:53 +0100

    Update commands.js
    changement pour les getbysel du login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,30 +1,27 @@
 Cypress.Commands.add('login', () => {
-    cy.visit('/login');
-    cy.get('#username').type('test2@test.fr');
-    cy.get('#password').type('testtest');
-    cy.get('[data-cy="login-submit"]').click();
-    cy.url().should('include', '/');
-  });
-  Cypress.Commands.add('getAuthToken', () => {
-    return 'fake-token-test'; // Remplace par une commande réelle si nécessaire
-  });
+  cy.visit('/login');
+  cy.getBySel('login-input-username').type('test2@test.fr');
+  cy.getBySel('login-input-password').type('testtest');
+  cy.getBySel('login-submit').click();
+  cy.url().should('include', '/');
+});
 Cypress.Commands.add("getBySel", (selector, ...args) => {
-  return cy.get(`[data-cy=${selector}]`, ...args);
-});  
+  return cy.get(`[data-cy="${selector}"]`, ...args); // Ajout de guillemets
+});
 Cypress.Commands.add('apiRequest', (method, url, body = null) => {
 const token = Cypress.env('authToken'); // Récupère le token stocké
 
 return cy.request({
-  method,
-  url: `${Cypress.env('apiBaseUrl')}${url}`,
-  headers: {
-    Authorization: `Bearer ${token}`,
-  },
-  body,
-  failOnStatusCode: false, // Permet d'inspecter les réponses même en cas d'erreur
+method,
+url: `${Cypress.env('apiBaseUrl')}${url}`,
+headers: {
+  Authorization: `Bearer ${token}`,
+},
+body,
+failOnStatusCode: false, // Permet d'inspecter les réponses même en cas d'erreur
 });
 });
 // Constante XSS
 export const XssTest = '<script>alert("XSS")</script>';
 
-  
+
