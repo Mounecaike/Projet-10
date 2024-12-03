166c42b9604daeec4f4fad2d66cd206c2bc264ac
Mounecaike <jordanl4213@gmail.com>
2024-12-03 16:45:59 +0100

    Update api.cy.js

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -5,38 +5,57 @@ describe('E2E Test - Ajouter un produit au panier', () => {
     cy.visit('/');
   });
 
-  it('Ajoute un produit via l\'UI et vérifie l\'appel API', () => {
-
-    // Interception de l'API avec vérification
-    cy.intercept('PUT', 'http://localhost:8081/orders/add').as('addProductAPI');
-    cy.login(); // Commande personnalisée pour se connecter
-       
-    // Vérifications préliminaires
-    cy.url().should('not.include', '/login'); // Vérifie qu'on n'est pas sur la page de login
-    cy.window().its('localStorage.authToken').should('exist'); // Vérifie que le token existe
-
-    // Navigation vers la page produit et ajout au panier
-    cy.visit('/products');
-    cy.get('[data-cy="product-link"]').eq(0).click();
-    cy.get('[data-cy="detail-product-add"]').click();
-
-    // Petit délai pour attendre la requête
-    cy.wait(1000);
-
-    // Intercepte et vérifie la requête API
-    cy.wait('@addProductAPI').then((interception) => {
-      // Ajout de logs pour vérifier ce qui est intercepté
-      cy.log('Interception : ', interception);
-
-      // Vérifications de l'API
-      expect(interception.response.statusCode).to.eq(200); // Vérifie le code réponse
-      expect(interception.request.body).to.have.property('product'); // Vérifie le body de la requête
-      expect(interception.request.body.product).to.eq(3); // Vérifie l'ID du produit
-      expect(interception.request.body.quantity).to.eq(1); // Vérifie la quantité
-    });
-
-    // Vérification du panier
+  it('ajout d\'un produit au panier', () => {
+    cy.login();
+    cy.getBySel('nav-link-logout').should('be.visible');
+    cy.intercept('GET', '/orders').as('getCart'); // vérifie la récuperation du panier en cours
+    cy.visit('/products/8');
+    cy.getBySel('detail-product-add').click();
     cy.visit('/cart');
-    cy.get('[data-cy="cart-line-quantity"]').should('have.length', 3); // Vérifie qu'il y a un seul élément
+    cy.wait(1000);
+    cy.wait('@getCart').then((interception) => {
+      expect(interception.response.statusCode).to.eq(200); // Vérifie le statut 200
+    });
   });
+
+  it('récuperer les avis', () => {
+    cy.login();
+    cy.getBySel('nav-link-logout').should('be.visible');
+    cy.intercept('GET', '/reviews').as('Getreviews');
+    cy.visit('/reviews');
+    cy.wait(1000)
+    cy.wait('@Getreviews').then((interception) => {
+      expect(interception.response.statusCode).to.eq(200);
+    });
+  });
+
+    it('page produits', () => {
+    cy.intercept('GET', '/products').as('getProducts');
+    cy.visit('/products');
+    cy.wait(1000)
+    cy.wait('@getProducts').then((interception) => { //verifier que l'on l'api renvoi bien la liste des produits
+      expect(interception.response.statusCode).to.eq(200);
+      expect(interception.response.body).to.be.an('array');
+    });
+    cy.getBySel('product-link').eq(0).click();
+    cy.url().should('include', '/products');
+    const selectors = [
+      'detail-product-img',
+      'detail-product-name',
+      'detail-product-description',
+      'detail-product-skin',
+      'detail-product-aromas',
+      'detail-product-ingredients',
+      'detail-product-form',
+      'detail-product-price',
+      'detail-product-stock',
+      'detail-product-quantity',
+      'detail-product-add',
+    ];
+    selectors.forEach((selectors) => {
+      cy.getBySel(selectors).should('be.visible');
+    });
+  })
+
 });
+
